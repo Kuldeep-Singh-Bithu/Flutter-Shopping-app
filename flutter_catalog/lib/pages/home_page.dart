8b0833cb341b48e7097e7b84e07fe250f75571dc
Kuldeep-Singh-Bithu <snipereagle99@gmail.com>
2021-09-14 01:40:36 +0530

    Day13 commit

--- flutter_catalog/lib/pages/home_page.dart
+++ flutter_catalog/lib/pages/home_page.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int age = 21;
+
   final String fname = "Kuldeep Singh";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+  loadData() async{
+   final  catalogJson =await rootBundle.loadString("assets/files/catalog.json");
+    //print(catalogJson);
+    final decodedData=jsonDecode(catalogJson);
+    var productsData=decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
